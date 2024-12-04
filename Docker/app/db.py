c1f3e54c7d702871cee7216210ae2f532d7c4101
Junyu Li <70296613+Junyu06@users.noreply.github.com>
2024-12-03 20:55:05 -0500

    gps

--- Docker/app/db.py
+++ Docker/app/db.py
@@ -25,8 +25,8 @@ class Photo(db.Model):
     folder_path = db.Column(db.String(1024))  # Folder organization (optional)
     thumbnail_path = db.Column(db.String(1024))  # Path to the generated thumbnail
     creation_date = db.Column(db.DateTime, default=datetime.utcnow)  # Automatically set to current time
-    gps_latitude = db.Column(db.Float)  # GPS latitude metadata
-    gps_longitude = db.Column(db.Float)  # GPS longitude metadata
+    gps_latitude = db.Column(db.Float, nullable=True)  # GPS latitude metadata
+    gps_longitude = db.Column(db.Float, nullable=True)  # GPS longitude metadata
     camera_model = db.Column(db.String(255))  # Camera model metadata
     focal_length = db.Column(db.Float)  # Focal length metadata
     lens_model = db.Column(db.String(255))  # Lens model metadata
@@ -35,5 +35,18 @@ class Photo(db.Model):
     album_id = db.Column(db.Integer, db.ForeignKey('album.id'), nullable=True)
     album = db.relationship('Album', backref='photos', lazy=True)
 
+    gps_cluster_id = db.Column(db.Integer, db.ForeignKey('gps_clusters.id'), nullable=True)
+    
     def __repr__(self):
         return f'<Photo {self.filename}>'
+
+class GPSCluster(db.Model):
+    __tablename__ = 'gps_clusters'
+
+    id = db.Column(db.Integer, primary_key=True)
+    cluster_latitude = db.Column(db.Float, nullable=False)
+    cluster_longitude = db.Column(db.Float, nullable=False)
+    photo_count = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<GPSCluster ({self.cluster_latitude}, {self.cluster_longitude})>'
